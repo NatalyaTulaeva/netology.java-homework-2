ba39a2adf431afb6e32d3baedf27296db2032b45
NatalyaTulaeva <n.tulaeva1@gmail.com>
2022-07-05 10:41:33 +0500

    java qa 2.1.1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 19800;
+        int bonusCost = 20;
+        int bonusAmount = ticketPrice / bonusCost;
+
+        System.out.println(bonusAmount);
+    }
+}
